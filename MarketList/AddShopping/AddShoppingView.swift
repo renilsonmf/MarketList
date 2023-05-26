7026dce2b228fc9d742ac2bad166be8ae28dab83
Renilson Moreira <renilsonmoreira_@hotmail.com>
2023-05-26 20:09:08 -0300

    add elements in shoppingView

--- MarketList/AddShopping/AddShoppingView.swift
+++ MarketList/AddShopping/AddShoppingView.swift
@@ -8,8 +8,40 @@
 import SwiftUI
 
 struct AddShoppingView: View {
+    
+    @State var productNameTextField: String = ""
+    @State var productQuantity: Int = 0
+    
     var body: some View {
-        Text("Add shopping view")
+        VStack(alignment: .center, spacing: 25) {
+            Image("supermarket")
+                .resizable()
+                .frame(width: 110, height: 100, alignment: .center)
+            Text("Adicionar item a lista de compras")
+                .font(Font.custom("Roboto-Regular", size: 20))
+            TextField("Digite algo", text: $productNameTextField)
+                .padding()
+                .background(Color(hex: "#F5F5F5"))
+                .cornerRadius(5)
+            Stepper(value: $productQuantity, in: 0...100) {
+                    Text("Quantidade = \(productQuantity)")
+                }
+
+            Button(action: didTapSave) {
+                Text("Salvar")
+                    .frame(maxWidth: .infinity)
+                    .foregroundColor(Color.white)
+            }
+            .frame(height: 48)
+            .background(Color(hex: "#8EA491"))
+            .cornerRadius(5)
+            .padding()
+
+        }.padding()
+    }
+    
+    private func didTapSave() {
+        print("deu bom")
     }
 }
 
